55604dbfc16f33b6b1d36e63ca2b0bb6cbea4dd2
Edson Rodriguez <erodriguez@quaxar.com>
2021-09-17 18:20:38 -0500

    animacion.js agregado

---
+++ animacion.js
@@ -0,0 +1,14 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+sr.reveal('.container', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+  });
